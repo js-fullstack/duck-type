baf90cb445dd2a8f1b76520a53cf4261ed82ac03
Shen Yu <yu.shen@hp.com>
2016-01-22 19:13:21 +0800

    implement build-in type,object

--- duck.js
+++ duck.js
@@ -8,6 +8,15 @@ var allTypes = {
 	},
 	number: function(n) {
 		return typeof n === 'number';
+	},
+	boolean: function(b) {
+		return typeof b === 'boolean';
+	},
+	object: function(o) {
+		return typeof o === 'object';
+	},
+	undefined: function(u) {
+		return typeof u === 'undefined';
 	}
 };
 
@@ -15,16 +24,25 @@ function Duck(target){
 	this.target = target;
 }
 
+
 Duck.prototype = {
 	is : function(type) {
 		if (typeof type === 'string') {
-			return allTypes[type](this.target);	
+			var validator = allTypes[type];
+			if(validator) {
+				return validator(this.target);
+			} else {
+				return false;
+			}	
+		} else if(typeof type === 'function') {
+			return this.target !== undefined && this.target !== null && this.target.constructor === type;
 		} else {
 			return false;
-		}
-		
+		}	
 	}
 };
+
+
 
 /**
 *	I am going to write many many code here...
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var duck = require('../duck');
+
 describe('duck-type', function() {
   	describe('base function', function () {
 	    it('should a function as node module', function () {
@@ -17,5 +18,93 @@ describe('duck-type', function() {
     		assert.equal(duck(123).is('number'), true);
     		assert.equal(duck('test').is('number'), false);
     	});
-  	}); 
+
+        it('boolean', function () {
+            assert.equal(duck(true).is('boolean'), true);
+            assert.equal(duck(123).is('boolean'), false);
+            assert.equal(duck(0).is('boolean'), false);
+        });
+
+        it('object', function () {
+            assert.equal(duck({}).is('object'), true);
+            assert.equal(duck([]).is('object'), true);
+            assert.equal(duck(null).is('object'), true);
+
+            assert.equal(duck(123).is('object'), false);
+            assert.equal(duck('sdfsdf').is('object'), false);
+            assert.equal(duck(true).is('object'), false);
+            assert.equal(duck(undefined).is('object'), false);
+        });
+
+        it('undefined', function () {
+            assert.equal(duck(undefined).is('undefined'), true);
+            assert.equal(duck(123).is('undefined'), false);
+            assert.equal(duck(0).is('undefined'), false);
+        });
+  	});
+
+    describe('build-in object', function () {
+        it('String', function () {
+            assert.equal(duck('test').is(String), true);
+            assert.equal(duck(123).is(String), false);
+        });
+
+        it('Number', function () {
+            assert.equal(duck(123).is(Number), true);
+            assert.equal(duck('test').is(Number), false);
+        });
+
+        it('Boolean', function () {
+            assert.equal(duck(true).is(Boolean), true);
+            assert.equal(duck(123).is(Boolean), false);
+            assert.equal(duck(0).is(Boolean), false);
+        });
+
+        it('Boolean', function () {
+            assert.equal(duck(true).is(Boolean), true);
+            assert.equal(duck(123).is(Boolean), false);
+            assert.equal(duck(0).is(Boolean), false);
+        });
+
+        it('Object', function () {
+            assert.equal(duck({}).is(Object), true);
+            
+            assert.equal(duck([]).is(Object), false);
+            assert.equal(duck('sdfsdf').is(Object), false);
+            assert.equal(duck(null).is(Object), false);
+            assert.equal(duck(123).is(Object), false);
+            assert.equal(duck(true).is(Object), false);
+            assert.equal(duck(undefined).is(Object), false);
+        });
+
+        it('Date', function () {
+            assert.equal(duck(new Date()).is(Date), true);
+        });
+
+        it('Array', function () {
+            assert.equal(duck([]).is(Array), true);
+            assert.equal(duck({}).is(Object), true);
+        });
+    });
+
+     describe('Customize object', function () {
+        it('base', function () {
+            function Person() {}
+            var p = new Person();
+            assert.equal(duck(p).is(Person), true);
+            assert.equal(duck(p).is('object'), true);
+            //assert.equal(duck(p).is(Object), true);
+        });
+
+        xit('inherited', function () {
+            function Person() {}
+            function Student() {}
+            Student.prototype = new Person();
+            var s = new Student();
+            assert.equal(duck(s).is(Student), true);
+            assert.equal(duck(s).is(Person), true);
+            assert.equal(duck(s).is('object'), true);
+            assert.equal(duck(s).is(Object), true);
+        });
+     }); 
 });
